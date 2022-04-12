290e33ed33a4695530dd34671f53074da1dbd89a
PriestMark <priestmark23@gmail.com>
2022-04-12 17:25:01 +0300

    removed commented code in main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import { SchemaComparator } from '@mikro-orm/postgresql';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { run } from './create-schema';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  run();
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
