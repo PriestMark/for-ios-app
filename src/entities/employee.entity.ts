13c9621a6c8a5994c05476557bad3e2972ed8d72
PriestMark <priestmark23@gmail.com>
2022-04-18 12:23:40 +0300

    entity defult was wrong

--- src/entities/employee.entity.ts
+++ src/entities/employee.entity.ts
@@ -11,12 +11,14 @@ export class Employee {
   firstName: string;
   @Property()
   lastName: string;
-  @Property({ nullable: true })
-  photoURL?: string;
   @Property({
     nullable: true,
     default:
       'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
+  })
+  photoURL?: string;
+  @Property({
+    nullable: true,
   })
   startDate: Date;
   @Property({ nullable: true })
