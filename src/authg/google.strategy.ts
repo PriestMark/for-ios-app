51381f570d0634a111ec7c7818bb876def813f57
PriestMark <priestmark23@gmail.com>
2022-04-18 14:22:30 +0300

    FIXED CONFIG

--- src/authg/google.strategy.ts
+++ src/authg/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: `${process.env.ADRESS}${process.env.PORT}/google/redirect`,
+      callbackURL: `${process.env.ADRESS}/google/redirect`,
       scope: ['email', 'profile'],
     });
   }
